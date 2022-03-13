337e1d325b5a3987c67553f355eef2d4eed4d057
Danh Đạt <datd@bap.jp>
2022-03-13 21:41:17 +0700

    update : update code

---
+++ app/Models/ProductCategory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductCategory extends Model
+{
+    use HasFactory;
+
+    protected $table = 'product_categories';
+    protected $fillable = [
+        'id',
+        'name',
+        'url',
+        'order',
+        'thumb_image',
+        'big_image',
+        'status',
+    ];
+
+    /**
+     * Get all of the products for the ProductCategory
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'category_id', 'id');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SlidersController;
+use App\Http\Controllers\ProductCategoriesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,19 @@ use App\Http\Controllers\SlidersController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+/** Slider Route **/
 Route::get('/slider/', [SlidersController::class, 'index']);
 Route::get('/slider/{id}', [SlidersController::class, 'show']);
 Route::delete('/slider/{id}', [SlidersController::class, 'destroy']);
 Route::post('/slider', [SlidersController::class, 'store']);
 Route::post('/slider/update', [SlidersController::class, 'update']);
 Route::post('/slider/status/update', [SlidersController::class, 'updateStatus']);
+
+/** Product Category **/
+Route::get('/product-category/', [ProductCategoriesController::class, 'index']);
+
+
+Route::get('/test-pusher', function(){
+    return view('test');
+});
